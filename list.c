d4694127c21cab1f9cbbacc93efa4b42a8be92b2
Flatw <flatwaze@yandex.ru>
2022-03-05 17:25:25 +0300

    Temporary memory have been cleaned

--- list.c
+++ list.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct Node{
+typedef struct Node
+{
     int data;
     struct Node *next;
-}Node;
+} Node;
 
 void print(Node *list);
 void push(Node **list, int data);
@@ -14,77 +15,102 @@ int length(Node *list);
 int findByIndex(Node *list, int index);
 int findByData(Node *list, int data);
 void insert(Node *list, int data, int index);
-int delete(Node *list, int index);
+int delete (Node *list, int index);
 
-
-void print(Node *list){
+void print(Node *list)
+{
     Node *tmp = list;
-    while (tmp != NULL){
+    while (tmp != NULL)
+    {
         printf("%d ", tmp->data);
         tmp = tmp->next;
     }
     printf("\n");
+    tmp = NULL;
+    free(tmp);
 }
 
-void push(Node **list, int data){
+void push(Node **list, int data)
+{
     Node *tmp = malloc(sizeof(Node));
     tmp->data = data;
     tmp->next = (*list);
     (*list) = tmp;
+    tmp = NULL;
+    free(tmp);
 }
 
-int isFull(Node *list){
-    if (list == NULL) return 0;
+int isFull(Node *list)
+{
+    if (list == NULL)
+        return 0;
     return 1;
 }
 
-int pop(Node **list){
+int pop(Node **list)
+{
     Node *tmp = (*list);
     int data = tmp->data;
-    (*list) = tmp->next; 
+    (*list) = tmp->next;
+    tmp = NULL;
     free(tmp);
     return data;
 }
 
-int length(Node *list){
+int length(Node *list)
+{
     int len = 0;
     Node *tmp = list;
-    while(tmp != NULL){
+    while (tmp != NULL)
+    {
         len++;
         tmp = tmp->next;
     }
+    tmp = NULL;
+    free(tmp);
     return len;
 }
 
-//starts from 1
-int findByIndex(Node *list, int index){
+// starts from 1
+int findByIndex(Node *list, int index)
+{
     int i = 0;
     Node *tmp = list;
-    while(i < index - 1){
+    while (i < index - 1)
+    {
         i++;
         tmp = tmp->next;
     }
     int data = tmp->data;
+    tmp = NULL;
+    free(tmp);
     return data;
 }
 
-//starts from 1, returns first Node with equal data
-int findByData(Node *list, int data){
+// starts from 1, returns first Node with equal data
+int findByData(Node *list, int data)
+{
     int i = 0;
     Node *tmp = list;
-    while (tmp != NULL){
+    while (tmp != NULL)
+    {
         i++;
-        if (tmp->data == data) return i;
+        if (tmp->data == data)
+            return i;
         tmp = tmp->next;
     }
+    tmp = NULL;
+    free(tmp);
     return -1;
 }
 
-//starts from 0, I gotta fix dis
-void insert(Node *list, int data, int index){
+// starts from 1
+void insert(Node *list, int data, int index)
+{
     int i = 0;
     Node *tmp = list;
-    while(i < index - 1){
+    while (i < index - 2)
+    {
         i++;
         tmp = tmp->next;
     }
@@ -92,12 +118,19 @@ void insert(Node *list, int data, int index){
     itmp->data = data;
     itmp->next = tmp->next;
     tmp->next = itmp;
+    tmp = NULL;
+    itmp = NULL;
+    free(tmp);
+    free(itmp);
 }
 
-int delete(Node *list, int index){
+// starts from 1
+int delete (Node *list, int index)
+{
     int i = 0;
     Node *tmp = list;
-    while (i < index - 2){
+    while (i < index - 2)
+    {
         i++;
         tmp = tmp->next;
     }
@@ -106,25 +139,36 @@ int delete(Node *list, int index){
     Node *itmp = tmp;
     int data = itmp->data;
     prev->next = itmp->next;
+    tmp = NULL;
+    itmp = NULL;
+    prev = NULL;
+    free(tmp);
+    free(itmp);
+    free(prev);
     return data;
 }
 
 int main()
 {
     Node *list = NULL;
-    Node a = {1}, b = {2}, c = {3}, d = {4};
-    list = &a;
-    a.next = &b;
-    b.next = &c;
-    c.next = NULL;
-    push(&list, 4);
-    pop(&list);
+    for (int i = 0; i < 10; i++)
+        push(&list, i);
+    printf("Pushed: ");
     print(list);
-    printf("%d\n", length(list));
-    printf("%d\n", findByIndex(list, 2));
-    printf("%d\n", findByData(list, 200));
-    insert(list, 5, 2);
-    delete(list, 2);
+    for (int i = 0; i < 3; i++)
+    {
+        printf("%d have been popped\n", pop(&list));
+    }
+
+    insert(list, 77, 5);
+    insert(list, 88, 4);
+    printf("Inserted: ");
+    print(list);
+
+    printf("%d have been deleted\n", delete (list, 3));
+    printf("%d have been deleted\n", delete (list, 2));
+
+    printf("Final result: ");
     print(list);
     return 0;
 }
